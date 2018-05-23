50c2347a9f0b23678aeb3a9dc7401d5fba49ff59
vivivale6-simulacroexamen-ln22hjah <vivivale6-simulacroexamen-ln22hjah@local>
2018-05-23 15:54:35 +0000

    ultimo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,12 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.linspace(0,2*np.pi)
+sen= np.sin(x)
+
+plt.plot(x,sen)
 
